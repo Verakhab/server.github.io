5f6e3e5518a799f4eed3bba560620a849f03d51a
Denis <47863172+Verakhab@users.noreply.github.com>
2020-06-29 16:10:37 +0300

    Correction error

--- app.js
+++ app.js
@@ -1,7 +1,6 @@
 const express = require('express');
 const path = require('path');
-const { userRouter } = require('./routes/users');
-const { usersRouter } = require('./routes/users');
+const usersRouter = require('./routes/users');
 const cardsRouter = require('./routes/cards');
 
 const { PORT = 3000 } = process.env;
@@ -10,13 +9,10 @@ const app = express();
 
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.use(usersRouter);
-app.use(userRouter);
-app.use(cardsRouter);
-app.use((req, res) => {
-  res.status(404).send({ message: 'Запрашиваемый ресурс не найден' });
+app.use('/', usersRouter);
+app.use('/', cardsRouter);
+app.use('*', (req, res) => {
+  res.status(404).send({ "message": "Запрашиваемый ресурс не найден" });
 });
 
-app.listen(PORT, () => {
-  console.log('Hello, word!!!');
-});
+app.listen(PORT);
