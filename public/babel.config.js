1b230b6f9f06b1c5f5851de496e91f4dc1c987b1
Denis Bakharev <denis.bahareff@yandex.ru>
2020-07-01 03:34:16 +0300

    Error correction public add 2

---
+++ public/babel.config.js
@@ -0,0 +1,19 @@
+const presets = [
+    [
+        "@babel/env",
+        {
+            targets: { 
+                    edge: "17",
+                    firefox: "50",
+                    chrome: "64",
+                    safari: "11.1",
+                    esmodules: true,
+                    ie: "11"
+            },
+            useBuiltIns: "usage",
+            corejs: "3.4.1",
+        }
+    ],
+];
+  
+module.exports = { presets };
