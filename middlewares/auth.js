495d5225d5ff48aa58b5ab43a3443ef7006d51bd
Denis Bakharev <denis.bahareff@yandex.ru>
2020-07-16 15:12:28 +0300

    Corrected authorization: add return send and message error

--- middlewares/auth.js
+++ middlewares/auth.js
@@ -3,14 +3,14 @@ const jwt = require('jsonwebtoken');
 module.exports = async (req, res, next) => {
   const { authorization } = req.headers;
   if (!authorization || !authorization.startsWith('Bearer ')) {
-    return res.status(401);
+    return res.status(401).send({ message: 'Требуется авторизация' });
   }
   const token = authorization.replace('Bearer ', '');
   let payload;
   try {
     payload = await jwt.verify(token, 'some-secret-key');
   } catch (err) {
-    return res.status(401).send(err.message);
+    return res.status(401).send({ message: 'Требуется авторизация' });
   }
   req.user = payload;
   next();
