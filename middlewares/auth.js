67cc8bae5424173c09c9b5cdf57c094551325a02
Denis Bakharev <denis.bahareff@yandex.ru>
2020-07-16 23:32:20 +0300

    Corrected auth.js

--- middlewares/auth.js
+++ middlewares/auth.js
@@ -1,14 +1,17 @@
 const jwt = require('jsonwebtoken');
+
+const { JWT_SECRET } = process.env;
+
 // eslint-disable-next-line consistent-return
 module.exports = async (req, res, next) => {
   const { authorization } = req.headers;
-  // if (!authorization || !authorization.startsWith('Bearer ')) {
-  //   return res.status(401).send({ message: 'Требуется авторизация' });
-  // }
-  // const token = authorization.replace('Bearer ', '');
+  if (!authorization || !authorization.startsWith('Bearer ')) {
+    return res.status(401).send({ message: 'Требуется авторизация' });
+  }
+  const token = authorization.replace('Bearer ', '');
   let payload;
   try {
-    payload = await jwt.verify(authorization, 'some-secret-key');
+    payload = await jwt.verify(token, JWT_SECRET);
   } catch (err) {
     return res.status(401).send({ message: 'Требуется авторизация' });
   }
