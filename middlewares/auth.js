7058004ca1bccbf8ace2d391666fe5f29b03d1a5
Denis Bakharev <denis.bahareff@yandex.ru>
2020-07-17 21:29:15 +0300

    corrected code errors and the rest can be better

--- middlewares/auth.js
+++ middlewares/auth.js
@@ -5,13 +5,13 @@ const { JWT_SECRET } = process.env;
 // eslint-disable-next-line consistent-return
 module.exports = async (req, res, next) => {
   const { authorization } = req.headers;
-  if (!authorization || !authorization.startsWith('Bearer ')) {
-    return res.status(401).send({ message: 'Требуется авторизация' });
-  }
-  const token = authorization.replace('Bearer ', '');
+  // if (!authorization || !authorization.startsWith('Bearer ')) {
+  //   return res.status(401).send({ message: 'Требуется авторизация' });
+  // }
+  // const token = authorization.replace('Bearer ', '');
   let payload;
   try {
-    payload = await jwt.verify(token, JWT_SECRET || 'some-secret-key');
+    payload = await jwt.verify(authorization, JWT_SECRET || 'some-secret-key');
   } catch (err) {
     return res.status(401).send({ message: 'Требуется авторизация' });
   }
