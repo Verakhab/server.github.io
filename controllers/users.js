cbb88916beb0b506694457d57e97bfc080bdb160
Denis Bakharev <denis.bahareff@yandex.ru>
2020-07-21 12:45:33 +0300

    corrected errors:)

--- controllers/users.js
+++ controllers/users.js
@@ -63,23 +63,14 @@ const createUser = async (req, res) => {
 
 const upUser = async (req, res) => {
   try {
-    if (!req.body.name || !req.body.about) {
-      throw new Error('Имя или информация о пользователе не заданы');
-    }
     const { name, about } = req.body;
     const user = await User.findByIdAndUpdate(req.user._id, { name, about },
       { new: true, runValidators: true })
       .orFail(new Error('Нет пользователя с таким id'));
     return res.status(200).send(user);
   } catch (err) {
-    if (err.message === 'Пароль не введён или не корректен') {
-      return res.status(400).send(err.message);
-    }
-    if (err.message === 'Нет пользователя с таким id') {
+    if (err.name === 'Error') {
       return res.status(404).send(err.message);
-    }
-    if (err.message === 'Имя или информация о пользователе не заданы') {
-      return res.status(400).send(err.message);
     }
     if (err.name === 'ValidationError') {
       return res.status(400).send(err.message);
@@ -90,24 +81,14 @@ const upUser = async (req, res) => {
 
 const upAvatar = async (req, res) => {
   try {
-    if (!req.body.avatar) {
-      throw new Error('Ссылка на аватар не задана');
-    }
     const { avatar } = req.body;
     const user = await User.findByIdAndUpdate(req.user._id, { avatar },
       { new: true, runValidators: true })
       .orFail(new Error('Нет пользователя с таким id'));
     return res.status(200).send(user);
   } catch (err) {
-
-    if (err.message === 'Нет пользователя с таким id') {
+    if (err.name === 'Error') {
       return res.status(404).send(err.message);
-    }
-    if (err.message === 'Ссылка на аватар не задана') {
-      return res.status(400).send(err.message);
-    }
-    if (err.message === 'Validation failed: avatar: Здесь должна быть ссылка') {
-      return res.status(400).send(err.message);
     }
     if (err.name === 'ValidationError') {
       return res.status(400).send(err.message);
