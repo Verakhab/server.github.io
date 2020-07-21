7dece5a49213aa47062543dbb2b8eaf5ef1d4ca8
Denis Bakharev <denis.bahareff@yandex.ru>
2020-07-21 23:03:36 +0300

    corrected last errors

--- controllers/users.js
+++ controllers/users.js
@@ -8,23 +8,23 @@ const { JWT_SECRET } = process.env;
 const getUsers = async (req, res) => {
   try {
     const usersAll = await User.find({})
-      .orFail(new Error(JSON.stringify({ message: 'Ошибка сервера' })));
+      .orFail(new Error('Ошибка сервера'));
     return res.status(200).send(usersAll);
   } catch (err) {
-    return res.status(500).send(err.message);
+    return res.status(500).send({ message: err.message });
   }
 };
 // eslint-disable-next-line consistent-return
 const getUser = async (req, res) => {
   try {
     const userId = await User.findById(req.params.userId)
-      .orFail(new Error(JSON.stringify({ message: 'Нет пользователя с таким id' })));
+      .orFail(new Error('Нет пользователя с таким id'));
     res.status(200).send(userId);
   } catch (err) {
     if (err.name === 'Error') {
-      return res.status(404).send(JSON.parse(err.message));
+      return res.status(404).send({ message: err.message });
     }
-    return res.status(500).send(err.message);
+    return res.status(500).send({ message: err.message });
   }
 };
 // eslint-disable-next-line consistent-return
@@ -48,9 +48,9 @@ const createUser = async (req, res) => {
       return res.status(409).send(err.message);
     }
     if (err.name === 'ValidationError') {
-      return res.status(400).send(err.message);
+      return res.status(400).send({ message: err.message });
     }
-    return res.status(500).send(err.message);
+    return res.status(500).send({ message: err.message });
   }
 };
 
@@ -59,16 +59,16 @@ const upUser = async (req, res) => {
     const { name, about } = req.body;
     const user = await User.findByIdAndUpdate(req.user._id, { name, about },
       { new: true, runValidators: true })
-      .orFail(new Error(JSON.stringify({ message: 'Нет пользователя с таким id' })));
+      .orFail(new Error('Нет пользователя с таким id'));
     return res.status(200).send(user);
   } catch (err) {
     if (err.name === 'Error') {
-      return res.status(404).send(JSON.parse(err.message));
+      return res.status(404).send({ message: err.message });
     }
     if (err.name === 'ValidationError') {
-      return res.status(400).send(err.message);
+      return res.status(400).send({ message: err.message });
     }
-    return res.status(500).send(err.message);
+    return res.status(500).send({ message: err.message });
   }
 };
 
@@ -77,16 +77,16 @@ const upAvatar = async (req, res) => {
     const { avatar } = req.body;
     const user = await User.findByIdAndUpdate(req.user._id, { avatar },
       { new: true, runValidators: true })
-      .orFail(new Error(JSON.stringify({ message: 'Нет пользователя с таким id' })));
+      .orFail(new Error('Нет пользователя с таким id'));
     return res.status(200).send(user);
   } catch (err) {
     if (err.name === 'Error') {
-      return res.status(404).send(JSON.parse(err.message));
+      return res.status(404).send({ message: err.message });
     }
     if (err.name === 'ValidationError') {
-      return res.status(400).send(err.message);
+      return res.status(400).send({ message: err.message });
     }
-    return res.status(500).send(err.message);
+    return res.status(500).send({ message: err.message });
   }
 };
 
@@ -94,14 +94,11 @@ const upAvatar = async (req, res) => {
 const login = async (req, res) => {
   try {
     const { email, password } = req.body;
-    if (!email || !password) {
-      throw new Error(JSON.stringify({ message: 'Пароль или email не заданы' }));
-    }
     const userFound = await User.findOne({ email }).select('+password')
-      .orFail(JSON.stringify({ message: 'Пароль или email не заданы' }));
+      .orFail(new Error('Пароль или email не заданы'));
     const isPass = await bcrypt.compare(password, userFound.password);
     if (!isPass) {
-      throw new Error(JSON.stringify({ message: 'Неверный пароль' }));
+      throw new Error('Пароль или email не заданы');
     }
     const token = jwt.sign({ _id: userFound._id },
       JWT_SECRET || 'some-secret-key',
@@ -112,7 +109,7 @@ const login = async (req, res) => {
     });
     return res.status(200).send({ token });
   } catch (err) {
-    res.status(401).send(err);
+    res.status(401).send({ message: err.message });
   }
 };
 
