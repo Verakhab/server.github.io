0cdd2fed7a46e6f1eaf4100708908ef160e9c358
Denis <47863172+Verakhab@users.noreply.github.com>
2021-02-21 13:27:11 +0300

    added cors routers users

--- routes/users.js
+++ routes/users.js
@@ -1,25 +1,26 @@
 const usersRouter = require('express').Router();
+const cors = require('cors');
 const { celebrate, Joi } = require('celebrate');
 const {
   getUser, getUsers, upUser, upAvatar,
 } = require('../controllers/users');
 
-usersRouter.get('/users', getUsers);
+usersRouter.get('/users', cors(), getUsers);
 
-usersRouter.get('/users/:userId', celebrate({
+usersRouter.get('/users/:userId', cors(), celebrate({
   params: Joi.object().keys({
     userId: Joi.string().hex().length(24),
   }),
 }), getUser);
 
-usersRouter.patch('/users/me', celebrate({
+usersRouter.patch('/users/me', cors(), celebrate({
   body: Joi.object().keys({
     name: Joi.string().required().min(2).max(30),
     about: Joi.string().required().min(2).max(30),
   }),
 }), upUser);
 
-usersRouter.patch('/users/me/avatar', celebrate({
+usersRouter.patch('/users/me/avatar', cors(), celebrate({
   body: Joi.object().keys({
     // eslint-disable-next-line no-useless-escape
     avatar: Joi.string().regex(/^(http(s)?:\/\/)(w{3}\.)?((\d+\.\d+\.\d+\.\d+)|(([A-Za-z\.-]{2,})\.([A-Za-z]{2,6})))((:\d{2,5})?\/?([\dA-Za-z\/]+#?))?/).required(),
