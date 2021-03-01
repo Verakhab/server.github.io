e01c6b9786f39126891306e7a053b1af60a7452c
Denis <47863172+Verakhab@users.noreply.github.com>
2021-03-01 23:43:52 +0300

    changed controller upavatar - return avatar, req.body, {avatar}

--- controllers/users.js
+++ controllers/users.js
@@ -65,10 +65,11 @@ const upUser = async (req, res, next) => {
 const upAvatar = async (req, res, next) => {
   try {
     const { avatar } = req.body;
-    const user = await User.findByIdAndUpdate(req.user._id, avatar,
-      { new: true, runValidators: true })
-      .orFail();
-    return res.status(200).send(user);
+    return res.status(200).send(avatar, req.body, { avatar });
+    // const user = await User.findByIdAndUpdate(req.user._id, avatar,
+    //   { new: true, runValidators: true })
+    //   .orFail();
+    // return res.status(200).send(user);
   } catch (err) {
     next(err);
   }
