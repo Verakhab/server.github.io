243834154e6f7aa7cd9f58ced2445cd9a5bcaac9
Denis <47863172+Verakhab@users.noreply.github.com>
2021-03-11 22:04:49 +0300

    added headers Access-Control-Allow-Credentials in cors midllware

--- middlewares/auth.js
+++ middlewares/auth.js
@@ -4,19 +4,19 @@ const Unauthorized = require('../errors/unauthorized-err');
 const { JWT_SECRET, NODE_ENV } = process.env;
 
 module.exports = async (req, res, next) => {
-  // const { authorization } = req.headers;
-  // if (!authorization || !authorization.startsWith('Bearer ')) throw new Unauthorized('Требуется авторизация');
-  // const token = authorization.replace('Bearer ', '');
-  // let payload;
-  // try {
-  //   payload = await jwt.verify(token, NODE_ENV === 'production' ? JWT_SECRET : 'some-secret-key');
-  // } catch (err) {
-  //   if (err.name === 'JsonWebTokenError') {
-  //     return next(new Unauthorized('Требуется авторизация'));
-  //   }
-  //   return next(err);
-  // }
-  // req.user = payload;
-  // return next();
-  res.send(req.headers);
+  const authoriz = req.headers.authorization;
+  if (!authoriz || !authoriz.startsWith('Bearer ')) throw new Unauthorized('Требуется авторизация');
+  const token = authoriz.replace('Bearer ', '');
+  let payload;
+  try {
+    payload = await jwt.verify(token, NODE_ENV === 'production' ? JWT_SECRET : 'some-secret-key');
+  } catch (err) {
+    if (err.name === 'JsonWebTokenError') {
+      return next(new Unauthorized('Требуется авторизация'));
+    }
+    return next(err);
+  }
+  req.user = payload;
+  return next();
+  // res.send(req.headers.authorization);
 };
