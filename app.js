774e377440976f07357e09daef55583c3b186589
Denis <47863172+Verakhab@users.noreply.github.com>
2021-03-11 19:45:15 +0300

    added method options in cors midllware

--- app.js
+++ app.js
@@ -35,7 +35,7 @@ app.use((req, res, next) => {
   if (allowedCors.includes(origin)) {
     res.header('Access-Control-Allow-Origin', origin);
     res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept, authorization');
-    res.header('Access-Control-Allow-Methods', 'GET,HEAD,PUT,PATCH,POST,DELETE');
+    res.header('Access-Control-Allow-Methods', 'GET,HEAD,PUT,PATCH,POST,DELETE,OPTIONS');
   }
 
   next();
