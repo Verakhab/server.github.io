769d8eb9bce3d72fce62353df125acd6d6057b90
Denis <47863172+Verakhab@users.noreply.github.com>
2021-03-11 21:38:27 +0300

    added headers Access-Control-Allow-Credentials in cors midllware

--- app.js
+++ app.js
@@ -36,6 +36,7 @@ app.use((req, res, next) => {
     res.header('Access-Control-Allow-Origin', origin);
     res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept, Authorization');
     res.header('Access-Control-Allow-Methods', 'GET,HEAD,PUT,PATCH,POST,DELETE,OPTIONS');
+    res.header('Access-Control-Allow-Credentials', 'true');
   }
 
   next();
